dd2cd8f504058ab284a54cecb9b0f464c80ecde2
Alexis Martinez <83483042+alx4329@users.noreply.github.com>
2022-11-09 19:37:09 -0300

    institutional width

--- client/src/components/Institutional/ComipInfo.jsx
+++ client/src/components/Institutional/ComipInfo.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { comipInfo } from "../../assets/data/institucional/comipInfo";
 import Footer from "../Footer/Footer";
 import './ComipInfo.css'
-
+import convenios01 from '../../assets/images/inst/interactivo.png'
 const ComipInfo = ()=>{
-    const convenios01 = "https://res.cloudinary.com/diqcwn9e9/image/upload/v1663104313/comip/convenios/convenios-01_tdcgda.png"
+    
     const HeadingPic = "https://res.cloudinary.com/diqcwn9e9/image/upload/v1663102914/comip/institucional/Portadas-02_1_ubegq5.png"
     const [open, setOpen] = useState(false)
     const handleOpen =() => setOpen(!open)
@@ -14,7 +14,6 @@ const ComipInfo = ()=>{
                 <img className="heading-pic" src={HeadingPic} alt="encabezado"/>
             </div>
             <div  className="comip-info-container" >
-                <img className="convenios-img" src={convenios01} alt="convenios-links" />
                 <div className="authorities-title" >{comipInfo.title}</div>
                 <div className="separator-auth-line" ></div>
                 <div className={open?"comip-info-text":"comip-info-text info-hidden "} >
@@ -27,12 +26,14 @@ const ComipInfo = ()=>{
                         })
                         
                     }
+                       
+                </div>
                 {
                     !open ? (
                         <div className="info-vermas" onClick={handleOpen} >Seguir leyendo...</div>
                     ):<div className="info-vermenos" onClick={handleOpen} >Ver menos</div>
                 }
-                </div>
+                <img className="convenios-img" src={convenios01} alt="convenios-links" />
                 <Footer/>
             </div>
         </>
--- client/src/containers/Institutional.jsx
+++ client/src/containers/Institutional.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import BottomCards from "../components/BottomCards";
 import './Institutional.css'
-// import HeadingPic from '../assets/images/encabezado-autoridades.png'
 import Authorities from "../components/Institutional/Authorities";
 import ComipInfo from "../components/Institutional/ComipInfo";
 import {  useParams } from "react-router-dom";
-import Rio from "./Rio";
 import Convenios from "../components/Institutional/Convenios";
 
 
