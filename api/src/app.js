cf305cd575eaf99cda7bc95d53ed00d59da94a65
Alexis Martinez <alexis4329@gmail.com>
2022-11-30 20:22:11 -0300

    init server

---
+++ api/src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const fileUpload = require('express-fileupload')
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+app.use(express.json()); //Used to parse JSON bodies
+app.use(cors());
+app.use( express.static( "public" ) );
+app.use(fileUpload({useTempFiles:true}))
+/*Routers used*/
+
+/* middlewares */
+
+
+/*Routes*/
+
+
+/* Invalid routes*/
+
+/* Invalid routes */
+app.use(function (_, res) {
+  res.json({ status: "inexistent route" });
+});
+
+module.exports = app;
