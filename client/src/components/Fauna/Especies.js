72bec9288fb6e8a092d2bb5d6c61cff5a6503690
Alexis Martinez <alexis4329@gmail.com>
2022-12-03 23:32:23 -0300

    update especies

--- client/src/components/Fauna/Especies.js
+++ client/src/components/Fauna/Especies.js
@@ -8,10 +8,14 @@ const Especies = () =>{
             <p>Esta región contiene una diversidad de peces muy importantes, contando con cerca de 200 especies, lo que representa aproximadamente un tercio del total de especies de toda la Cuenca del Plata.</p>
             <p>La diversidad de especies es aprovechada por el hombre haciendo diferentes usos sobre una veintena de peces. Estos usos son principalmente recreativos como la pesca recreativa y los concursos de pesca, pero a la vez esta forma de uso del recurso genera un ingreso económico importante para la región, tanto por la actividad turística de la pesca recreativa, como por todo el abastecimiento que el turismo necesita. Se incluye entre estos últimos una pesca de peces para carnadas que abastece a la pesca deportiva.</p>
             <p>Por último, la pesca comercial está limitada espacialmente solo a las orillas de los ríos Paraná y Paraguay con la salvedad que en costas de la Provincia de Corrientes no está permitida. Esta pesca es considerada artesanal ya que sus artes de pesca y embarcaciones son habitualmente confeccionadas a mano.</p>
+            <br/>
             <div className="subsection" >
                 <div className="subsection-title" >Especies de relevancia</div>
                 <p>El criterio de selección se basa en las especies más capturadas en la región, asi como algunas de características biológicas relevantes.</p>
                 <p >La descripción de cada especie es modificada en parte del libro: <a className="subsection-especies-link" target='_blank' rel="noreferrer nofollow" href="https://comip.org.ar/wp-content/uploads/2020/11/GuiadelospecesdelParqueNacionalPre-Delta__AlmirnCasciottaCiotekyGiorgis2015_compressed-1.pdf"><b><i>Guia de los peces del Parque Nacional Pre-Delta </i></b><i>/ Adriana Almirón; Jorge Casciotta; Liliana Ciotek; Pablo Giorgis. – 2da ed. – Ciudad Autónoma de Buenos Aires: Administracion de Parques Nacionales, 2015. </i></a></p>
+                <br/>
+                <br/>
+
                 <EspeciesList/>
             </div>
             <div className="esp-libros-container" >
--- client/src/components/Fauna/FaunaConv.js
+++ client/src/components/Fauna/FaunaConv.js
@@ -40,13 +40,17 @@ const FaunaConv = () =>{
                 <div className="subsection-title" >La Importancia de los Recursos Ícticos</div>
                 <br/>
                 <p className="subsection-text" >La conservación de la riqueza ictícola del Río Paraná es un objetivo central de la COMIP. A su vez, es una responsabilidad de los organismos municipales, provinciales, nacionales e internacionales pertinentes. Y su protección debe ser una condición primordial para quienes se relacionan con el Río.</p>
+                <br/>
                 <p><strong>› Convenio sobre conservación de Fauna Ictica:</strong></p>
                 <p>La COMIP es Secretaria Permanente del Convenio sobre conservación y desarrollo de los recursos ícticos en los tramos limítrofes de los ríos Paraná y Paraguay.</p>
                 <p>Este convenio cuenta con una normativa estrictamente binacional. Las provincias que se encuentran dentro de la competencia de la COMIP, tienen una normativa específica provincial sobre el tema. Dicha información se puede consultar en el Marco Legal Regulatorio de la Pesca Marítima, Continental y la Acuicultura Argentina, del Consejo Federal de Inversiones.</p>
+                <br/>
                 <p><strong>› Especies:</strong></p>
                 <p>El Río Paraná cuenta con una diversidad de peces, cerca de 200 especies, que representan aproximadamente un tercio del total de especies de toda la Cuenca del Plata. El criterio de selección, se basa en las especies de mayor captura en la región, así como algunas de características biológicas relevantes.</p>
+                <br/>
                 <p><strong>› Pesca Recreativa:</strong></p>
                 <p>El proyecto de Evaluación de la Pesca Recreativa en el Río Paraná en los tramos comunes, busca establecer una metodología que permita obtener información valida acerca de la actividad de pesca deportiva. Se apoya en muestreos periódicos con el fin de generar las bases para el desarrollo de políticas de manejo regional del recurso pesquero</p>
+                <br/>
                 <p><strong>› Estudios</strong></p>
                 <p>Tareas de recopilación de datos, estudios y análisis científicos sobre distintos aspectos relacionados a la vida del Río. Dichos estudios contienen datos propios, de la Entidad Binacional Yacyretá, Universidad del Nordeste, Universidad Nacional de Misiones.</p>
                 <br/>
@@ -55,12 +59,14 @@ const FaunaConv = () =>{
                 <p>La República Argentina y la República del Paraguay en 1996, suscribieron el Convenio sobre conservación y desarrollo de los recursos ícticos en los tramos limítrofes de los Ríos Paraná y Paraguay, en busca de preservar y conservar los recursos Icticos en dichos ríos, y establecer criterios racionales de pesca, evitar el deterioro ambiental y contaminación de ambos Ríos y sus ecosistemas</p>
                 <p>Estos instrumentos han sido ratificados por la Ley N°1.171 de la República del Paraguay y aprobados por la Ley de Argentina N°25.105.</p>
                 <p>La COMIP, actúa como Secretaria Permanente del Comité Coordinador. Dicho Comité, se encuentra constituido por dos delegaciones que es presidida por el Ministerio de Relaciones Exteriores de Paraguay y de Argentina, y seis representantes de cada país. A su vez, es asistido por un Consejo Asesor, el cual se encuentra representado por técnicos y científicos en materia de pesca y conservación de la Fauna Íctica, con el objeto de formular propuestas y recomendaciones al Comité.</p>
+                <br/>
                 <p><strong>Misión y funciones de la Secretaría Permanente:</strong></p>
                 <p>›Mantener el inventario y la evaluación estadística de los recursos vivos y de la pesca, a través del procesamiento, análisis y la publicación de información tal como capturas, esfuerzos de pesca y otros datos provistos por organismos afines.</p>
                 <p>›Organizar el intercambio de información entre las partes contratantes concerniente a la ejecución de ese Convenio.</p>
                 <p>›Preparar y elevar propuestas al Comité Coordinador, relativas a la investigación científica en el área del Convenio.</p>
                 <p>›Coordinar la planificación de proyectos de investigación y desarrollo tecnológico sobre la pesca en los Ríos Paraná y Paraguay para ser realizados en forma conjunta o separada por los organismos competentes de las partes contratantes.</p>
                 <p>›Coordinar el control y efectuar el seguimiento permanente de las acciones y medidas dispuestas por el Comité Coordinador.</p>
+                <br/>
                 <p><strong>Misión y funciones del Comité Coordinador:</strong></p>
                 <p>›Control de pesca y preservación del recurso íctico.</p>
                 <p>›Regulación de la pesca, la conservación y desarrollo de la Fauna Ictica.</p>
@@ -76,6 +82,9 @@ const FaunaConv = () =>{
                 <p>›La convocatoria a las reuniones del Consejo Asesor estará a cargo de la Secretaria Permanente.</p>
                 <p>›Formulará y elevará a requerimiento del Comité Coordinador las propuestas y recomendaciones para el cumplimiento de las misiones y funciones previstas en el Art. XI del Convenio.</p>
             </div>
+            <br/>
+            <br/>
+            <br/>
             <div className="faunaconv-buttons" >
                 <div onClick={()=>goTo("estudiosdelconvenio")} className="faunaconv-button" >ESTUDIOS DEL CONVENIO</div>
                 <div onClick={()=>goTo("normaBilaterales")} className="faunaconv-button" >NORMATIVAS BILATERALES</div>
