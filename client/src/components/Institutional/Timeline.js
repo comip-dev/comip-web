8e5d08d9f2ab9a5bbf7c5e36e76465677b0d4e18
Alexis Martinez <alexis4329@gmail.com>
2022-12-05 01:16:22 -0300

    timeline-institucional

---
+++ client/src/components/Institutional/Timeline.js
@@ -0,0 +1,156 @@
+import React from 'react'
+import { Chrono } from "react-chrono";
+import './Timeline.css'
+const Timeline = () =>{
+    const myList = [
+        {
+            year:'1971',
+            // text:'CONVENIO PARA EL ESTUDIO DEL APROVECHAMIENTO DE LOS RECURSOS DEL RÍO PARANA',
+            text:'Convenio para el estudio del aprovechamiento de los recursos del Río Paraná',
+            link:'https://comip.org.ar/quienessomos/#:~:text=CONVENIO%20PARA%20EL%20ESTUDIO%20DEL%20APROVECHAMIENTO%20DE%20LOS%20RECURSOS%20DEL%20R%C3%8DO%20PARANA'
+        },
+        {
+            year:'1979',
+            text:'CONVENIO TRIPARTITO',
+            link:'Convenio Tripartito'
+        },
+        {
+            year:'1989',
+            // text:'ACUERDO POR CANJE DE NOTAS SOBRE FAUNA ÍCTICA Y CALIDAD DE AGUA',
+            text:'Acuerdo por canje de notas sobre Fauna Íctica y Calidad de Agua',
+            link:'https://www.comip.org.ar/wp-content/uploads/2017/05/1989-ACUERDO_FAUNA.pdf'
+        },
+        {
+            year:'1992',
+            double:true,
+            list:[
+                {
+                    // text:'ACUERDO POR CAMBIO DE NOTAS DESIGNANDO A LA COMIP COMO COMISIÓN ADMINISTRADORA DEL TRAMO CONTIGUO DEL RÍO PARANÁ',
+                    text:'Acuerdo por cambio de notas designado a la COMIP como comisión administradora del tramo contiguo del Río Paraná',
+                    link:'https://www.comip.org.ar/wp-content/uploads/2017/05/1992-ACUERDO-Comisión-administradora.pdf',
+                },
+                {
+                    text:'Acuerdo por canje de notas sobre autoridad de control de la Zona de Reserva Íctica',
+                    // text:'ACUERDO POR CANJE DE NOTAS SOBRE AUTORIDAD DE CONTROL DE LA ZONA DE RESERVA ÍCTICA',
+                    link:'https://www.comip.org.ar/wp-content/uploads/2018/08/Nota-Reversal-N°7-1992.pdf',
+                },
+            ]
+        },
+        {
+            year:'1998',
+            text:'Reglamento técnico-administrativo',
+            link:'https://www.comip.org.ar/wp-content/uploads/2017/05/Reglamento-técnico-administrativo.pdf'
+        },
+        {
+            year:'2000',
+            // text:'MEMORANDUM DE ENTENDIMIENTO ENTRE LOS GOBIERNOS DE LA REPÚBLICA ARGENTINA Y LA REPÚBLICA DEL PARAGUAY SOBRE EL PROYECTO CORPUS CHRISTI',
+            text:'Memorándum de entendimiento entre los gobiernos de la República Argentina y la República del Paraguay sobre el proyecto Corpus Christi',
+            link:'https://www.comip.org.ar/wp-content/uploads/2018/08/MEMORANDUM-CORPUS.pdf'
+        },
+        {
+            year:'2001',
+            double:true,
+            list:[
+                {
+                    // text:'ACUERDO DE SEDE ENTRE LA COMIP Y LA REPÚBLICA ARGENTINA',
+                    text:'Acuerod de sede entre la COMIP y la República Argentina',
+                    link:'https://www.comip.org.ar/wp-content/uploads/2018/08/Acuerdo-de-sede-COMIP.pdf',
+                },
+                {
+                    // text:'ACUERDO DE SEDE ENTRE LA COMIP Y LA REPÚBLICA DEL PARAGUAY – LEY Nº 1971',
+                    text:'Acuerdo de sede entre la COMIP y la República del Paraguay - Ley N° 1971',
+                    link:'https://www.comip.org.ar/wp-content/uploads/2017/10/ley-py.pdf',
+                },
+            ]
+        },
+        
+        {
+            year:'2005',
+            text:'Estatuto del Tribunal Arbitral',
+            link:'https://www.comip.org.ar/wp-content/uploads/2017/05/Estatuto-del-tribunal-arbitral.pdf'
+        },
+        {
+            year:'2006',
+            double:true,
+            list:[
+                {text:'El Congreso Argentino aprueba el acuerdo de sede - Ley N° 26.187',
+                // {text:'EL CONGRESO ARGENTINO APRUEBA EL ACUERDO DE SEDE – LEY Nº 26.187',
+                link:'https://www.comip.org.ar/wp-content/uploads/2017/10/ley-ba.pdf'},
+                {text:'Memorandum de entendimiento entre la República del Paraguay y la República Argentina',
+                // {text:'MEMORANDUM DE ENTENDIMIENTO ENTRE LA REPÚBLICA DEL PARAGUAY Y LA REPÚBLICA ARGENTINA',
+                link:'https://www.comip.org.ar/wp-content/uploads/2017/05/Memorandum-de-entendimiento-entre-la-república-del-paraguay-y-la-república-argentina.pdf'},
+            ]
+        },
+        
+    ]
+    return(
+        <>
+        <Chrono
+            mode="HORIZONTAL"
+            cardPositionHorizontal='TOP'
+            disableClickOnCircle={false}
+            scrollable={false}
+            lineWidth='2'
+            // cardWidth='1000rem'
+            cardHeight='300'
+            borderLessCards
+            theme={{
+                primary:"#969696",secondary:"#05488C",
+                titleColorActive: 'white',
+                }}
+            fontSizes={{
+                    cardSubtitle: '0.85rem',
+                    cardText: '0.8rem',
+                    cardTitle: '1rem',
+                    title: '1.1rem',
+                }}
+                classNames={{
+                    card: 'my-card',
+                    // cardMedia: 'my-card-media',
+                    // cardSubTitle: 'my-card-subtitle',
+                    // cardText: 'my-card-text',
+                    // cardTitle: 'my-card-title',
+                    // controls: 'my-controls',
+                    title: 'my-title',
+                    titleActive:'my-titleActive'
+                }}
+            items={myList.map((item,i)=>{
+            return{title:item.year,}
+
+        })}
+        >
+        {
+        myList.map((item,i)=>{
+            return(
+                <div className={item.double?'card-tl-container db-tl':'card-tl-container'} >
+                {
+                    item.double ? (
+                        <>
+                        {
+                            item.list.map(conv=>{
+                                return <div className='tl-item-container' >
+                                        <div className='separator-tl-line' ></div>
+                                        <a className='tl-convenio-link' href={conv.link} target='blank' >{conv.text}</a>
+                                    </div>
+                            })
+                        }
+                        </>
+                    ):
+                    <div className='tl-item-container' >
+                        <div className='separator-tl-line' ></div>
+                        <a className='tl-convenio-link' href={item.link} target='blank' >{item.text}</a>
+                    </div>
+                }
+                </div>
+                )
+
+        })
+
+        }
+
+        </Chrono> 
+        </>
+    )
+}
+
+export default Timeline
