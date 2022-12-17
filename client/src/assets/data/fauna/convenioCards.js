6881509c453e7e1e5379a3959ae25f265f58f2eb
Alexis Martinez <83483042+alx4329@users.noreply.github.com>
2022-12-17 18:23:08 -0300

    add formosa picture

--- client/src/assets/data/fauna/convenioCards.js
+++ client/src/assets/data/fauna/convenioCards.js
@@ -21,7 +21,7 @@ export const cards = [
     },
     {
         zone:"Formosa",
-        img:"/images/Fauna/1_Convenio/Imagen 4.jpg",
+        img:"/images/Fauna/1_Convenio/Imagen 5.jpg",
         pdf:"https://comip.org.ar/wp-content/uploads/2022/11/Dispo-Veda-pesca-102-22-1.pdf"
     },
 ]
