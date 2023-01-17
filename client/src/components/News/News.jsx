fd4663ea5e0fafcce14c738327fc41f54e1ec3ec
Alexis Martinez <83483042+alx4329@users.noreply.github.com>
2023-01-17 11:04:13 -0300

    Update News.jsx

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -2,19 +2,28 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { bloginfo } from "../../assets/data/bloginfo";
 import useWindowSize from "../../hooks/useWindowSize";
+import { getNews } from "../../services/news";
 import New from "./New";
 import './News.css'
 
 const News = () =>{
     const navigate = useNavigate()
     const windowSize = useWindowSize()
+    const [noticias,setNoticias] = React.useState([])
+    React.useEffect(()=>{
+        const fetchData = async() =>{
+            const news = await getNews(1)
+            setNoticias(news.docs)
+        }
+        fetchData()
+    },[])
     return(
         <div className="news-section" >
             <div className="news-container" >
                 <div className="news-title" >Novedades</div>
                 <div className={windowSize.innerWidth>720?"news-list":'res-news-list'} >
                     {
-                        bloginfo?.map(item=>{
+                        noticias.length>0 && noticias.slice(0,4).map(item=>{
                             return <New 
                                     img={item.img}
                                     title={item.title}
